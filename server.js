c54dd53547a343d40e7a37962b499f26e4e673da
nannal <dabbott@localhost.localdomain>
2019-01-12 00:00:22 +0200

    Review this - cleaned up

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 const express = require("express");
 var path = require("path");
 const bodyParser = require("body-parser");
-var cmds = require("./script.js");
-var avalon = require("./avalon.js")
+var {cmds, avalon} = require("./script.js");
 
 
-
-//HTTP or HTTPS on remote avalon node
-var avaHttps = process.env.AVA_HTTPS || 0
-var httpMethod = "http"
-if (avaHttps==0){
- var httpMethod = "https"
-}
 //config for port our service will use
 var port = process.env.PORT || 8080
-
 
 
 const app = express();
@@ -25,12 +16,10 @@ app.get("/", function (req, res){
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-console.log(avalon.randomNode());
 
 app.post("/createAccount", function (req, res){
 
-
-
+      // callback function
       function cb(err, block) {
          if (err) {
           console.log(err);
